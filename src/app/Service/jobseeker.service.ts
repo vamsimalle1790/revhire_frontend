15c098e5abcdc843f0ca47052d91942217d86be2
vamsimalle1790 <132806447+vamsimalle1790@users.noreply.github.com>
2024-05-30 13:47:13 +0530

    Update jobseeker.service.ts

--- src/app/Service/jobseeker.service.ts
+++ src/app/Service/jobseeker.service.ts
@@ -8,7 +8,7 @@ import { Application } from '../Model/application';
   providedIn: 'root'
 })
 export class JobseekerService {
-  base_url = "http://vamsimalleuserservices:8082/";
+  base_url = "http://vamsimalleuserservices.jobservices.svc:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJobsByUserId(id:Number):Observable <any>{
