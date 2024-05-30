c935ecacf313f393dc7464b1b4eb26ace33153db
vamsimalle1790 <132806447+vamsimalle1790@users.noreply.github.com>
2024-05-30 12:54:33 +0530

    Update employer.service.ts

--- src/app/Service/employer.service.ts
+++ src/app/Service/employer.service.ts
@@ -9,7 +9,7 @@ import { Application } from '../Model/application';
 })
 export class EmployerService {
 
-  base_url = "aa5908059c31f4345bfbc62895807390-301179049.us-east-1.elb.amazonaws.com:8082/";
+  base_url = "http://vamsimalleuserservices:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJob(Id:number):Observable <any>{
